f064a4b62f24b004ff6bf53f82a387b6fbfc9b6f
Mavlonyusuf <mavlonjonyusupov1@gmail.com>
2024-12-18 15:10:26 +0500

    vue-project created

---
+++ src/components/counter.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p>{{ sanoq }}</p>
+    <button @click="increment">oshirish</button>
+    <button @click="increment">kamaytirish</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+let sanoq = ref(0);
+function increment() {
+  sanoq.value++;
+}
+</script>
+
+<style scoped>
+button {
+  background-color: darkgreen;
+  color: white;
+  padding: 11px 33px;
+  border: 1px solid transparent;
+  border-radius: 8px;
+  cursor: pointer;
+}
+</style>
